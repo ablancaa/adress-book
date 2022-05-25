d9b65ea53ed182efec3542034ca692ea26d65845
ablancaa <88708431+ablancaa@users.noreply.github.com>
2022-05-25 22:56:38 +0200

    Cambio de posición del botón de borrado

--- src/components/CardContact.vue
+++ src/components/CardContact.vue
@@ -10,9 +10,9 @@
                         <div class="contacto-privado" v-show="address.private"><img src="@/assets/candado.png" /></div>
                         <h2 class="tituloSlide">{{ address.name }} {{ address.lastName }}</h2>
                         <i class="fa fa-user-circle" aria-hidden="true"></i>
-                        <button v-show="store.logged" class="delete-contact" @click="deleteAddress" >
-                          <img src="../assets/delete-button.svg" alt="Delete Contact" />
-                        </button>
+                        <!-- <button v-show="store.logged" class="delete-contact" @click="deleteAddress" > -->
+                          <!-- <img src="../assets/delete-button.svg" alt="Delete Contact" /> -->
+                        <!-- </button> -->
                     </div>
                 </div>
             </div>
@@ -32,6 +32,9 @@
       <path d="M3.654 1.328a.678.678 0 0 0-1.015-.063L1.605 2.3c-.483.484-.661 1.169-.45 1.77a17.568 17.568 0 0 0 4.168 6.608 17.569 17.569 0 0 0 6.608 4.168c.601.211 1.286.033 1.77-.45l1.034-1.034a.678.678 0 0 0-.063-1.015l-2.307-1.794a.678.678 0 0 0-.58-.122l-2.19.547a1.745 1.745 0 0 1-1.657-.459L5.482 8.062a1.745 1.745 0 0 1-.46-1.657l.548-2.19a.678.678 0 0 0-.122-.58L3.654 1.328zM1.884.511a1.745 1.745 0 0 1 2.612.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z"/>
     </svg> -- {{ address.phone }}</span>
     </div>
+    <button v-show="store.logged" class="delete-contact" @click="deleteAddress" >
+      <img src="../assets/delete-button.svg" alt="Delete Contact" />
+    </button>
                     <p></p>
                 </div>
             </div>
@@ -111,7 +114,7 @@ export default {
 }
 .delete-contact {
   position: absolute;
-  top: 10px;
+  top: 170px;
   right: 10px;
   background: transparent;
   border: 0;
@@ -124,7 +127,7 @@ export default {
 
 .contacto-privado {
 position: absolute;
-top: 5px;
+top: 10px;
 left:15px;
 background: transparent;
 border: 0;
@@ -142,6 +145,11 @@ width: 25px;
 
 
 /* OTRO ESTILO */
+.container{
+ margin: 0 auto;
+ height: auto;
+ width: 300px;
+}
 .container .card .icon  {
     position: absolute;
     top: 0;
