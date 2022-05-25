0d7c9b6de0e6f7a54e3d1b2e05b1388d2b82b0be
ablancaa <88708431+ablancaa@users.noreply.github.com>
2022-05-25 11:30:58 +0200

    Elimino array usuario

--- src/store/user.js
+++ src/store/user.js
@@ -3,14 +3,6 @@ import { defineStore } from 'pinia'
 export const userStore = defineStore('user', {
     id: 'user',
     state: () => ({
-        usuario: [{
-            logged: false,
-            name: '',
-            lastName: '',
-            email: '',
-            token: '',
-            listaPrivada: [],  
-        }],
         logged: false,
         name: '',
         lastName: '',
