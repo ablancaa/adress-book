d84bf0b6ce2389fd76f7d0c4c7cb1f49aa0fffd5
ablancaa <88708431+ablancaa@users.noreply.github.com>
2022-05-25 23:03:10 +0200

    Modificación de CSS para iconos tarjetas

--- src/components/CardContact.vue
+++ src/components/CardContact.vue
@@ -20,7 +20,9 @@
                 <div class="content">
 
     <div class="contact-info">
-      <span class="contact-address">{{ address.address }} {{ address.city }} {{ address.state }} - {{ address.zip }} - {{address.country}}</span>
+      <span class="contact-address"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-signpost-2" viewBox="0 0 16 16">
+  <path d="M7 1.414V2H2a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h5v1H2.5a1 1 0 0 0-.8.4L.725 8.7a.5.5 0 0 0 0 .6l.975 1.3a1 1 0 0 0 .8.4H7v5h2v-5h5a1 1 0 0 0 1-1V8a1 1 0 0 0-1-1H9V6h4.5a1 1 0 0 0 .8-.4l.975-1.3a.5.5 0 0 0 0-.6L14.3 2.4a1 1 0 0 0-.8-.4H9v-.586a1 1 0 0 0-2 0zM13.5 3l.75 1-.75 1H2V3h11.5zm.5 5v2H2.5l-.75-1 .75-1H14z"/>
+</svg> -- {{ address.address }} {{ address.city }} {{ address.state }} - {{ address.zip }} - {{address.country}}</span>
     </div><!-- FIN contact-info -->
     <div class="contact-info">
       <span class="contact-info-value"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope margen" viewBox="0 0 16 16">
@@ -98,7 +100,7 @@ export default {
 
 .contact-address {
   text-align: left;
-  font-size: 20px;
+  font-size: 18px;
 }
 .contact-contact {
   font-size: 16px;
