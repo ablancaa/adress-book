9198711ce938facecf5f3ed44534000aa1a19bd5
ablancaa <88708431+ablancaa@users.noreply.github.com>
2022-05-27 20:47:31 +0200

    Cambios en presentación de usuario

--- src/components/LoginLogout.vue
+++ src/components/LoginLogout.vue
@@ -6,8 +6,7 @@
       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope margen" viewBox="0 0 16 16">
              <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4Zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2Zm13 2.383-4.708 2.825L15 11.105V5.383Zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741ZM1 11.105l4.708-2.897L1 5.383v5.722Z"/>
              </svg>  {{ store.email }}</p></div>
-      <!-- <div v-show="store.email"><p class="user">{{ store.email }}</p></div> -->
-      <!-- <div v-show="store.email" class="user lineUp"><p>¡Hola! - {{ store.email }} </p></div> -->
+             <!-- <div  v-show="store.email" class="userToken"><p class="lineUp">{{store.token}}</p></div> -->
         <button v-show="!store.email" class="login-button" @click="showLogin">Login</button>
         <button v-show="store.email" class="logout-button" @click="logout">Logout</button>
   </div>
@@ -70,9 +69,15 @@ export default {
   font-weight: bold;
   font-size: 20px;
 }
-.user{
+.user {
   float: right;
-  margin-right: -475px;
+  margin-right: -470px;
+  margin-top:0px
+}
+.userToken {
+  float: right;
+  margin-right: -390px;
+  margin-bottom: -30px;
 }
 .header .user {
   display: flex;
