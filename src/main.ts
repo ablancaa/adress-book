2c2e52ff9ea301e6d7b0b179b5f304d75636f71f
ablancaa <88708431+ablancaa@users.noreply.github.com>
2022-05-30 12:45:03 +0200

    Añado libreria sweetAlert para mensajes al usuario

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 const pinia = createPinia();
 import './registerServiceWorker'
-
+import  Swal  from 'sweetalert2';
 //import router from './router'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
@@ -11,4 +11,4 @@ import VueAxios from 'vue-axios'
 
 
 
-createApp(App).use( pinia, VueAxios, axios).mount('#app')
+createApp(App).use( pinia,Swal, VueAxios, axios).mount('#app')
